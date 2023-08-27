90d50d1891754e4b8219d9a5bd79999d4509bd77
Chuong Huynh <hnchuong@gmail.com>
2023-08-27 08:02:02 +0700

    [345264] add setting suffix_attribute - also remoe unused reserved_user_input

--- lib/stringex/acts_as_url/adapter/base.rb
+++ lib/stringex/acts_as_url/adapter/base.rb
@@ -188,6 +188,15 @@ module Stringex
 
         def modify_base_url
           root = instance.send(settings.attribute_to_urlify).to_s
+
+          suffix = ''
+          suffix_attribute = settings.suffix_attribute
+          if suffix_attribute
+            suffix_attribute = suffix_attribute.call(instance) if suffix_attribute.lambda?
+            suffix = instance.send(suffix_attribute) if suffix_attribute
+          end
+          root += "-#{suffix}" if suffix.present?
+
           self.base_url = root.to_url(configuration.string_extensions_settings)
         end
 
