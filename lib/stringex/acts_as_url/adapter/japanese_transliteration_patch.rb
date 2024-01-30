0e7e456fa0b0bcf44d6f0d67bd9231b4e96b3a87
Al Palaran <aljade1902@gmail.com>
2024-01-30 10:55:30 +0000

    add base

--- lib/stringex/acts_as_url/adapter/japanese_transliteration_patch.rb
+++ lib/stringex/acts_as_url/adapter/japanese_transliteration_patch.rb
@@ -1,53 +1,55 @@
 module Stringex
   module ActsAsUrl
     module Adapter
-      module JapaneseTransliterationPatch
-        JAP_CHAR_REGEXP ||= /[\p{Han}\p{Hiragana}\p{Katakana}]+/.freeze
-        KAKASI_ERR ||= 'Kakasi not installed, skipping kanji transliteration'.freeze
-        ICONV_ERR ||= 'Iconv not installed, skipping kanji transliteration'.freeze
+      module Base
+        module JapaneseTransliterationPatch
+          JAP_CHAR_REGEXP ||= /[\p{Han}\p{Hiragana}\p{Katakana}]+/.freeze
+          KAKASI_ERR ||= 'Kakasi not installed, skipping kanji transliteration'.freeze
+          ICONV_ERR ||= 'Iconv not installed, skipping kanji transliteration'.freeze
 
-        def modify_base_url
-          root = instance.send(settings.attribute_to_urlify).to_s
+          def modify_base_url
+            root = instance.send(settings.attribute_to_urlify).to_s
 
-          root = transliterate_kanji(root)
-          self.base_url = root.to_url(configuration.string_extensions_settings)
-        end
+            root = transliterate_kanji(root)
+            self.base_url = root.to_url(configuration.string_extensions_settings)
+          end
 
-        private
+          private
 
-        def transliterate_kanji(string)
-          return string unless transliterate_kanji?
-          return string unless contains_japanese?(string)
-          return string unless iconv_installed?
-          return string unless kakasi_installed?
+          def transliterate_kanji(string)
+            return string unless transliterate_kanji?
+            return string unless contains_japanese?(string)
+            return string unless iconv_installed?
+            return string unless kakasi_installed?
 
-          safe_string = Shellwords.escape(string)
+            safe_string = Shellwords.escape(string)
 
-          cmd = "echo '#{safe_string}'"\
-            ' | iconv -f utf8 -t eucjp'\
-            ' | kakasi -i euc -w | kakasi -i euc -Ha -Ka -Ja -Ea -ka'
+            cmd = "echo '#{safe_string}'"\
+              ' | iconv -f utf8 -t eucjp'\
+              ' | kakasi -i euc -w | kakasi -i euc -Ha -Ka -Ja -Ea -ka'
 
-          `#{cmd}`
-        end
+            `#{cmd}`
+          end
 
-        def transliterate_kanji?
-          instance.respond_to?(:transliterate_kanji?) && instance.transliterate_kanji?
-        end
+          def transliterate_kanji?
+            instance.respond_to?(:transliterate_kanji?) && instance.transliterate_kanji?
+          end
 
-        def contains_japanese?(string)
-          string =~ JAP_CHAR_REGEXP
-        end
+          def contains_japanese?(string)
+            string =~ JAP_CHAR_REGEXP
+          end
 
-        def kakasi_installed?
-          `echo test | kakasi` == "test\n"
-        rescue StandardError
-          Rails.logger.error(KAKASI_ERR)
-        end
+          def kakasi_installed?
+            `echo test | kakasi` == "test\n"
+          rescue StandardError
+            Rails.logger.error(KAKASI_ERR)
+          end
 
-        def iconv_installed?
-          `iconv --version`
-        rescue StandardError
-          Rails.logger.error(ICONV_ERR)
+          def iconv_installed?
+            `iconv --version`
+          rescue StandardError
+            Rails.logger.error(ICONV_ERR)
+          end
         end
       end
     end
